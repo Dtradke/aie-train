e740ff7b3560eb7940ec0aebdd90301354e6e66d
Dave Radke <dtradke@uwaterloo.ca>
2021-06-22 08:10:08 -0700

    new train teams mach

---
+++ src/train_teams_mach.py
@@ -0,0 +1,39 @@
+import time
+
+import ray
+from ray.rllib.agents import ppo
+from ray.rllib.models import ModelCatalog
+
+from aie.env_conf import ENV_CONF_TEAMS_MACH
+from rl.conf import get_base_ppo_conf
+from rl.models.tf.fcnet import FCNet
+
+
+def get_conf():
+    lr = 3e-5
+
+    return {
+        **get_base_ppo_conf(num_workers=24),
+        'lr': lr,
+        'env_config': ENV_CONF_TEAMS_MACH,
+    }
+
+
+def run():
+
+
+    ModelCatalog.register_custom_model('my_model', FCNet)
+
+    trainer = ppo.PPOTrainer(config=get_conf())
+
+    t = time.monotonic()
+    while True:
+        trainer.train()
+        checkpoint = trainer.save()
+        print(time.monotonic() - t, 'checkpoint saved at', checkpoint)
+        t = time.monotonic()
+
+
+if __name__ == '__main__':
+    ray.init()
+    run()
