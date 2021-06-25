4cc773d7b39f5793be84e5909c33f24dae922927
Dave Radke <dtradke@uwaterloo.ca>
2021-06-25 07:28:05 -0700

    train default teams

--- src/train_teams.py
+++ src/train_teams.py
@@ -4,7 +4,7 @@ import ray
 from ray.rllib.agents import ppo
 from ray.rllib.models import ModelCatalog
 
-from aie.env_conf import ENV_CONF_TEAMS_DEFAULT_RS
+from aie.env_conf import ENV_CONF_TEAMS_DEFAULT_RS, ENV_CONF_PLANNER_TEAMS_DEFAULT_RS
 from rl.conf import get_base_ppo_conf
 from rl.models.tf.fcnet import FCNet
 
@@ -13,7 +13,7 @@ def get_conf():
     lr = 3e-5
 
     return {
-        **get_base_ppo_conf(num_workers=24),
+        **get_base_ppo_conf(num_workers=40),
         "lr_schedule": [
             [0, lr],
             [10_000_000, lr],
